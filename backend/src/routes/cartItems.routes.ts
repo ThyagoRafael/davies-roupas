2729414361be1b1f2756a32a2114ff6d43ceec13
ThyagoRafael <thyagorafael57@gmail.com>
2026-04-20 21:05:23 -0300

    feat: adicionar rotas para funções dos itens do carrinho

---
+++ backend/src/routes/cartItems.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { CartItemController } from "../controllers/cartItem.controller.js";
+
+const cartItemRoutes = Router();
+const cartItemController = new CartItemController();
+
+cartItemRoutes.post("/:userId", cartItemController.addToCart);
+cartItemRoutes.put("/:userId", cartItemController.updateCartItem);
+cartItemRoutes.delete("/:userId", cartItemController.deleteCartItem);
+
+export { cartItemRoutes };
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,6 +4,7 @@ import { userRoutes } from "./user.routes.js";
 import { productRoutes } from "./product.routes.js";
 import { productImageRoutes } from "./productImages.routes.js";
 import { cartRoutes } from "./cart.routes.js";
+import { cartItemRoutes } from "./cartItems.routes.js";
 
 const routes = Router();
 
@@ -11,5 +12,6 @@ routes.use("/users", userRoutes);
 routes.use("/products", productRoutes);
 routes.use("/products/:productId", productImageRoutes);
 routes.use("/carts", cartRoutes);
+routes.use("/cart-items", cartItemRoutes);
 
 export { routes };
